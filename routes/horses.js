a6845b327a839669bb772bddb4575469edf0230f
topaloff <33520153+topaloff@users.noreply.github.com>
2020-02-07 10:47:28 +0100

    ajout controller

--- routes/horses.js
+++ routes/horses.js
@@ -1,86 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const checkAuth = require('../middleware/checkAuth');
-const Horse = require('../models/Horse');
 
-// GET all horses
-router.get('/', (req,res,next) => {
-    Horse.find((err, horses) => {
-        if(err){
-            return next(err);
-        }
-        res.json(horses);
-    })
-})
+const horse_controller = require('../controllers/horse.controller.js')
 
-//GET SINGLE HORSE
-router.get('/:id', (req, res, next) =>{
-    const id = req.params.id;
-    Horse.findById(id, (err, horse) => {
-        if(err) return next(err)
-        res.json(horse);
-    })
-})
-
-//GET PRIZE OF HORSE
-router.get('/prize/:id', (req, res, next) =>{
-    const id = req.params.id;
-    Horse.findById(id, (err, horse) => {
-        if(err) return next(err)
-        res.json(horse.prize);
-    })
-})
-
-//GET FULL DETAIL OF HORSE WITH OWNER
-router.get('/full/:id', (req, res, next) =>{
-    const id = req.params.id;
-    Horse.findById(id, (err, horse) => {
-        if(err) return next(err)
-        Owner.findById(horse.owner, (err, owner) => {
-            if(err) return next(err)
-            horse.owner = owner;
-            res.json(horse);
-        })
-    })
-})
-
-
-//ADD HORSE
-router.post('/add', checkAuth, (req, res, next)=>{
-    Horse.find({certificat: req.body.certificat})
-    .then(data => {
-        if(data.length !== 0){
-            res.status(409).json({message: 'Le cheval existe deja'} );
-            res.end();
-        }
-    })
-    .catch(err => res.json(err))
-
-    const newHorse = req.body;
-    Horse.create(newHorse, (err, horse) => {
-        if(err) return next(err)
-        res.json(horse)
-    })
-})
-
-//DELETE HORSE
-router.delete('/delete/:id',checkAuth, (req, res, next) =>{
-    const id = req.params.id;
-    Horse.findByIdAndDelete(id,(err,horse)=>{
-        if(err) return next(err)
-        res.json(horse);
-    })
-})
-
-// //UPDATE HORSE
-router.put('/:id', checkAuth, (req, res, next) => {
-    const id = req.params.id;
-    const updatedHorse = req.body;
-    Horse.findByIdAndUpdate(id, updatedHorse, (err, horse) => {
-        if(err) return next(err)
-        res.json(horse);
-    })
-})
+router.get('/', horse_controller.horse_list);
+router.get('/:id', horse_controller.horse_detail);
+router.get('/prize/:id', horse_controller.horse_prize);
+router.get('/full/:id', horse_controller.horse_full);
+router.post('/add', checkAuth, horse_controller.horse_add);
+router.delete('/delete/:id', checkAuth, horse_controller.horse_delete);
+router.put('/:id', checkAuth, horse_controller.horse_update);
+router.put('/:id', checkAuth, horse_controller.horse_update);
+router.post('/prize/:id', checkAuth, horse_controller.horse_add_prize);
 
 module.exports = router;
--- routes/owners.js
+++ routes/owners.js
@@ -1,63 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const checkAuth = require('../middleware/checkAuth');
-const Owner = require('../models/Owner');
+const owner_controller = require('../controllers/owner.controller');
 
-// GET all owners
-router.get('/', (req,res,next) => {
-    Owner.find((err, owners) => {
-        if(err){
-            return next(err);
-        }
-        res.json(owners);
-    })
-})
-
-//GET SINGLE OWNER
-router.get('/:id', (req, res, next) =>{
-    const id = req.params.id;
-    Owner.findById(id, (err, owner) => {
-        if(err) return next(err)
-        res.json(owner);
-    })
-})
-
-//ADD OWNER
-router.post('/add', checkAuth, (req, res, next)=>{
-    Owner.find({email: req.body.email})
-    .then(data => {
-        if(data.length !== 0){
-            res.status(409).json({message: 'Le owner existe deja'} );
-            res.end();
-        }
-    })
-    .catch(err => res.json(err))
-
-    const newOwner = req.body;
-    Owner.create(newOwner, (err, owner) => {
-        if(err) return next(err)
-        res.json(owner)
-    })
-})
-
-//DELETE OWNER
-router.delete('/delete/:id',checkAuth, (req, res, next) =>{
-    const id = req.params.id;
-    Owner.findByIdAndDelete(id,(err,owner)=>{
-        if(err) return next(err)
-        res.json(owner);
-    })
-})
-
-// //UPDATE OWNER
-router.put('/:id', checkAuth, (req, res, next) => {
-    const id = req.params.id;
-    const updatedOwner = req.body;
-    Owner.findByIdAndUpdate(id, updatedOwner, (err, owner) => {
-        if(err) return next(err)
-        res.json(owner);
-    })
-})
+router.get('/', owner_controller.owner_list);
+router.get('/:id', owner_controller.owner_detail);
+router.post('/add', checkAuth, owner_controller.owner_add);
+router.delete('/delete/:id', checkAuth, owner_controller.owner_delete);
+router.put('/:id', checkAuth, owner_controller.owner_update);
 
 module.exports = router;
